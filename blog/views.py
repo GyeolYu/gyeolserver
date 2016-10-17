5561ea8e1b4a09cebaf8bb56050265d379c85842
Gyeol Yu <yugul3291@ncsoft.com>
2016-10-17 17:18:12 +0900

    Add templates and index page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.shortcuts import render
+from django.template import Context, Template
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
+def main(request):
+    return render(request, 'blog/index.html')
 # Create your views here.
